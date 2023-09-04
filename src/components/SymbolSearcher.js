bd7b8ea39832e0e96b3d818d4e0a504469e5c50d
PeDro0210 <avi23089@uvg.edu.gt>
2023-09-03 23:54:44 -0600

    Scroll bar and class update

--- src/components/SymbolSearcher.js
+++ src/components/SymbolSearcher.js
@@ -1,37 +1,46 @@
 import '../styles/SymbolSearcher.css';
-import {useState} from 'react';
+import {Component} from 'react';
 
-function SymbolSearcher(){
-    const [symbol, setSymbol] = useState('');
-    const [data, setData] = useState([]);
 
-    const handleSearch = () => {
-        fetch(`http://127.0.0.1:5000//SymbolSearcher/<${symbol}>`)
+class SymbolSearcher extends Component {
+    // Good, now this is a class and now I understand how the use state works
+    constructor(props) {
+        super(props);
+        this.state = {
+            symbol: '',
+            data: []
+        };
+    }
+
+    handleSearch = () => {
+        fetch(`http://127.0.0.1:5000//SymbolSearcher/<${this.state.symbol}>`)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                setData(data);
+                this.setState({ data: data });
             })
             .catch(error => {
                 console.log(error);
             });
     }
 
-    return(
-        <div>
-            <input id='symbol-input' type='text' placeholder='Write Symbol' className='SymbolSeacher-textbox' value={symbol} onChange={(e) => setSymbol(e.target.value)} />
-            <button id='symbol-searcher-button' className='symbolSearcher-button' onClick={handleSearch}>Search</button>
-            <div className = 'ShowSymbolSearcher'> 
-                {Array.isArray(data) && data.map((item,index) => (
-                    console.log(item),
-                    <button  key={index} data-name={item['2. name']}>
-                    {item['1. symbol']}, {item['2. name']}
-                </button>
-                ))}
-                
+    render() {
+        return (
+            <div>
+                <input id='symbol-input' type='text' placeholder='Write Symbol' className='SymbolSeacher-textbox' value={this.state.symbol} onChange={(e) => this.setState({ symbol: e.target.value })} />
+                <button id='symbol-searcher-button' className='symbolSearcher-button' onClick={this.handleSearch}>Search</button>
+                <div className='ShowSymbolSearcher'>
+                    {Array.isArray(this.state.data) && this.state.data.map((item, index) => (
+                        console.log(item),
+                        <button key={index} data-name={item['2. name']}>
+                            {item['1. symbol']}, {item['2. name']}
+                        </button>
+                    ))}
+
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default SymbolSearcher;
