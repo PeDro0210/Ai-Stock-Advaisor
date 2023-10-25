25048c9ace6843f6952edd7431d9652216475dbc
JavierCC <cif23079@uvg,edu.gt>
2023-10-24 21:39:54 -0600

    Comments API Comments

--- src/components/SymbolSearcher.js
+++ src/components/SymbolSearcher.js
@@ -1,22 +1,22 @@
 import '../styles/SymbolSearcher.css';
-import {Component} from 'react';
+import { Component } from 'react';
 import StockGraph from './Graph.js';
 import Chat from './Chat.js';
 
 class SymbolSearcher extends Component {
-    // TODO: Comment in to this shit.
     constructor(props) {
         super(props);
         this.state = {
             symbol: '',
             data: [],
-            FoundSymbol:'',
+            FoundSymbol: '',
             showGraph: false
         };
     }
 
+    // Este método realiza una búsqueda utilizando la API y actualiza el estado con los resultados.
     HandleSearch = () => {
-        fetch(`http://127.0.0.1:5000//SymbolSearcher/<${this.state.symbol}>`)
+        fetch(`http://127.0.0.1:5000/SymbolSearcher/${this.state.symbol}`)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
@@ -27,30 +27,36 @@ class SymbolSearcher extends Component {
             });
     }
 
+    // Este método se llama cuando se hace clic en un botón de símbolo y actualiza el estado con el símbolo seleccionado.
     SelectSymbol = (e) => {
-        console.log("Symbol Catch: "+e);
-        this.setState({FoundSymbol: e });
+        console.log("Symbol Catch: " + e);
+        this.setState({ FoundSymbol: e });
     }
 
+    // Este método se llama cuando se hace clic en un botón "AddGraph" y muestra el gráfico.
     AddGraph = () => {
-        this.setState({showGraph: true});
+        this.setState({ showGraph: true });
     }
 
     render() {
         return (
             <div>
-
+                {/* Input para ingresar el símbolo */}
                 <input
                     id="symbol-input"
                     type="text"
                     placeholder="Write Symbol"
-                    className="SymbolSeacher-textbox"
+                    className="SymbolSearcher-textbox"
                     onChange={(e) => this.setState({ symbol: e.target.value })}
-                    onKeyDown={(e) => {{if (e.key === 'Enter') 
-                        {this.HandleSearch(); 
-                        document.getElementById("symbol-input").value = "";}}}}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            this.HandleSearch();
+                            document.getElementById("symbol-input").value = "";
+                        }
+                    }}
                 />
 
+                {/* Botón de búsqueda */}
                 <button
                     id="symbol-searcher-button"
                     className="symbolSearcher-button"
@@ -59,6 +65,7 @@ class SymbolSearcher extends Component {
                     Search
                 </button>
 
+                {/* Mostrar los resultados de la búsqueda */}
                 <div className="ShowSymbolSearcher">
                     {Array.isArray(this.state.data) &&
                         this.state.data.map((item, index) => {
@@ -76,28 +83,26 @@ class SymbolSearcher extends Component {
                                 </button>
                             );
                         })}
-
                 </div>
-                {
-                    this.state.showGraph && (
-                        <>
-                            <StockGraph
-                                key={this.state.FoundSymbol}
-                                FoundSymbol={this.state.FoundSymbol}
-                            />
-                            <Chat
-                                key={this.state.FoundSymbol}
-                                FoundSymbol={this.state.FoundSymbol}
-                                data={this.state.data}
-                            />
-                            {console.log("Create components" + this.state.FoundSymbol)}
-                        </>
-                    )
-                }
-            </div>
 
+                {/* Mostrar el gráfico y el chat si showGraph es true */}
+                {this.state.showGraph && (
+                    <>
+                        <StockGraph
+                            key={this.state.FoundSymbol}
+                            FoundSymbol={this.state.FoundSymbol}
+                        />
+                        <Chat
+                            key={this.state.FoundSymbol}
+                            FoundSymbol={this.state.FoundSymbol}
+                            data={this.state.data}
+                        />
+                        {console.log("Create components" + this.state.FoundSymbol)}
+                    </>
+                )}
+            </div>
         );
     }
 }
 
-export default SymbolSearcher;  
+export default SymbolSearcher;
